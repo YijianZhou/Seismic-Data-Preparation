2fe95293df45f97ab4ae37f7138233b34354ed69
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2019-09-03 15:50:17 +0800

    Add files via upload

--- sac.py
+++ sac.py
@@ -8,7 +8,7 @@ cut sac trace
 merge traces
 """
 
-def cut(fpath, b, e, out_path):
+def cut(fpath, b, e, outpath):
     """ set [t0, t1] --> cut stream
     faster in long continuous records
     b, e: second relative to hdeader b
@@ -19,7 +19,7 @@ def cut(fpath, b, e, out_path):
     s += "cut %s %s \n" %(b, e)
     s += "r %s \n" %(fpath)
     s += "ch allt (0-&1,b&) iztype IB \n"
-    s += "w %s \n" %(out_path)
+    s += "w %s \n" %(outpath)
     s += "q \n"
     p.communicate(s.encode())
 
@@ -27,10 +27,11 @@ def cut(fpath, b, e, out_path):
 def merge(fpaths, out_path):
     """ merge sac files
     """
+    if len(fpaths)==0: return
     if len(fpaths)==1: os.rename(fpaths[0], out_path); return
     p = subprocess.Popen(['sac'], stdin=subprocess.PIPE)
     s = "wild echo off \n"
-    print('merge sac files {}'.format(fpaths))
+    print('merge sac files to {}'.format(out_path))
     for i,fpath in enumerate(fpaths):
         if i==0: s += "r %s \n" %(fpath)
         else:    s += "r more %s \n" %(fpath)
@@ -72,7 +73,7 @@ def ch_event(fpath, evlo, evla, evdp, mag, tn=[]):
     s += "ch evlo %s evla %s \n" %(evlo, evla)
     s += "ch evdp %s \n" %(evdp)
     s += "ch mag %s \n" %(mag)
-    for i,ti in enumerate(tn): 
+    for i,ti in enumerate(tn):
         s += "ch t%s %s \n" %(i,ti)
     s += "wh \n"
     s += "q \n"
@@ -80,15 +81,16 @@ def ch_event(fpath, evlo, evla, evdp, mag, tn=[]):
 
 
 
-""" Format Changing
+""" Format CHanging
 seed to sac
 miniseed/ mseed to sac
 with IRIS tool: mseed2sac & rdseed
 """
 
-def seed2sac(fpath):
-    subprocess.call(['rdseed', '-df', fpath])
+def seed2sac(fpath, out_dir=None):
+    if out_dir: subprocess.call(['rdseed', '-dfq', fpath, out_dir])
+    else: subprocess.call(['rdseed', '-df', fpath])
 
 def mseed2sac(fpath):
-    subprocess.call(['mseed2sac', fpath])
+    subprocess.call(['mseed2sac', '-O', fpath])
 
