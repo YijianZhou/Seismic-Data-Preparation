b80823894f5dcfb56804c7bf315dee5387f0c17d
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2022-04-19 19:01:57 -0700

    Update plot_data-continuity.py

--- plot_data-continuity.py
+++ plot_data-continuity.py
@@ -6,18 +6,17 @@ from obspy import read, UTCDateTime
 import numpy as np
 
 # i/o paths
-stream_paths = glob.glob('/data2/Ridgecrest/*/*HZ*')
+stream_paths = glob.glob('/data/Example_data/*/*.*Z.SAC') # root/yyyymmdd/date.net.sta.chn
 title = 'Example Data Continuity Plot'
-fout = 'output/example_data_continuity.pdf'
-# fig params
+fout = 'output/eg_data-continuity.pdf'
+# fig config
 fig_size = (12,16)
 fsize_label = 14
 fsize_title = 18
-mark_size = 100
+marker_size = 100
 
-# stats for stations
+# count existing files
 sta_dict = {}
-print('making station dict')
 for stream_path in stream_paths:
     fname = os.path.split(stream_path)[-1]
     date, net, sta, chn = fname.split('.')[0:4]
@@ -27,17 +26,15 @@ for stream_path in stream_paths:
     else:
         sta_dict[net_sta].append(UTCDateTime(date).date)
 
-print('plot continuity')
 plt.figure(figsize=fig_size)
 ax = plt.gca()
 sta_dict_keys = sorted(list(sta_dict.keys()))
 for i, sta_key in enumerate(sta_dict_keys):
     x1 = sta_dict[sta_key]
-    plt.scatter(x1, [i]*len(x1), marker='s', s=mark_size)
+    plt.scatter(x1, [i]*len(x1), marker='s', s=marker_size)
 plt.setp(ax.xaxis.get_majorticklabels(), fontsize=fsize_label, rotation=20)
 plt.yticks(list(range(len(sta_dict))), sta_dict_keys, fontsize=fsize_label)
 plt.title(title, fontsize=fsize_title)
 plt.grid(True)
 plt.tight_layout()
 plt.savefig(fout)
-plt.show()
