3b046415cfdeab201be7dc57422e3012b54ac5df
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2022-07-07 12:26:40 -0700

    Create data-clean_gap.py

---
+++ data-clean_gap.py
@@ -0,0 +1,42 @@
+""" Data clean: data gaps that are zero-filled
+    Zero-filled gaps will become glitches after rmean; rtr; taper; filter. 
+    This code change the zero-filled fata into average fill
+    If the data is merged with average fill strategy, don't need to run this code.
+"""
+import os, shutil, glob
+import numpy as np
+from obspy import read, UTCDateTime
+
+# i/o paths
+data_dirs = sorted(glob.glob('/data/Example_data/*'))
+bak_dir = '/data/Example_bak'
+# data clean params
+min_gap_npts = 10 
+num_gap_bak = 50 # if num_gap>num_gap_bak, backup
+
+def ave_fill(st):
+    data = st[0].data
+    npts = len(data)
+    gap_idx = np.where(data==0)[0]
+    gap_list = np.split(gap_idx, np.where(np.diff(gap_idx)!=1)[0] + 1)
+    gap_list = [gap for gap in gap_list if len(gap)>=min_gap_npts]
+    for gap in gap_list:
+        idx0, idx1 = max(0, gap[0]-1), min(npts-1, gap[-1]+1)
+        ave_fill = np.mean([data[idx0], data[idx1]])
+        data[gap[0]:gap[-1]+1] = ave_fill
+    st[0].data = data
+    return st, len(gap_list)
+
+for data_dir in data_dirs:
+    print('cleaning', data_dir)
+    st_paths = sorted(glob.glob(os.path.join(data_dir,'*')))
+    for st_path in st_paths:
+        try: st = read(st_path)
+        except: print('error in reading file', st_path); continue
+        fname = os.path.basename(st_path)
+        bak_path = os.path.join(bak_dir, fname)
+        st, num_gap = ave_fill(st)
+        if num_gap>0: print('average filled %s gaps: %s'%(num_gap, st_path))
+        if num_gap>=num_gap_bak: shutil.copy(st_path, bak_path)
+        st.write(st_path)
+
