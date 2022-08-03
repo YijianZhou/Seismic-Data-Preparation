5afc35e2578a54b879b933f84a25e74a1604e4b1
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2022-08-03 11:46:09 -0700

    Update data-clean_gap.py

--- data-clean_gap.py
+++ data-clean_gap.py
@@ -14,17 +14,21 @@ bak_dir = '/data/Example_bak'
 min_gap_npts = 10 
 num_gap_bak = 50 # if num_gap>num_gap_bak, backup
 
-def ave_fill(st):
+def fill_gap(st):
     data = st[0].data
     npts = len(data)
     gap_idx = np.where(data==0)[0]
     gap_list = np.split(gap_idx, np.where(np.diff(gap_idx)!=1)[0] + 1)
     gap_list = [gap for gap in gap_list if len(gap)>=min_gap_npts]
-    for gap in gap_list:
+    num_gap = len(gap_list)
+    for ii,gap in enumerate(gap_list):
         idx0, idx1 = max(0, gap[0]-1), min(npts-1, gap[-1]+1)
-        delta = (data[idx1] - data[idx0]) / (idx1-idx0)
-        interp_fill = np.array([data[idx0] + ii*delta for ii in range(idx1-idx0)])
-        data[idx0:idx1] = interp_fill
+        if ii<num_gap-1: idx2 = min(idx1+(idx1-idx0), gap_list[ii+1][0])
+        else: idx2 = min(idx1+(idx1-idx0), npts-1)
+        if idx2==idx1+(idx1-idx0): data[idx0:idx1] = data[idx1:idx2]
+        else:
+            num_tile = int(np.ceil((idx1-idx0)/(idx2-idx1)))
+            data[idx0:idx1] = np.tile(data[idx1:idx2], num_tile)[0:idx1-idx0]    st[0].data = data
     st[0].data = data
     return st, len(gap_list)
 
@@ -36,7 +40,7 @@ for data_dir in data_dirs:
         except: print('error in reading file', st_path); continue
         fname = os.path.basename(st_path)
         bak_path = os.path.join(bak_dir, fname)
-        st, num_gap = ave_fill(st)
+        st, num_gap = fill_gap(st)
         if num_gap>=num_gap_bak: shutil.copy(st_path, bak_path)
         if num_gap>0: 
             print('average filled %s gaps: %s'%(num_gap, st_path))
